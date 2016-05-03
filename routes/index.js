2a67b84d5e0e57624b91f0f7d43327d830e5f92d
Kimber Lynch <kmlynch714@gmail.com>
2016-05-03 12:42:10 -0400

    enable retweeting and fix order

--- routes/index.js
+++ routes/index.js
@@ -11,38 +11,58 @@ var bodyParser = require('body-parser');
 
 module.exports = function (io, client) {
 
-  function promiseQuery(query, valueArray, func){
-    return new Promise(function (resolve, reject,){
-      client.query(query, valueArray, func)
+  function promiseQuery(query, valueArray){
+    return new Promise(function (resolve, reject){
+      client.query(query, valueArray, function(err, result){
+        if(err) reject(err);
+        else resolve(result);
+      })
     })
   }
+  // function promiseQuery(query, valueArray){
+  //   return new Promise(function (resolve, reject){
+  //     client.query(query, valueArray, func)
+  //   })
+  // }
 
-  function returnQueryObj(err, result){
-    if(err) reject(err);
-    else resolve(result);
-  }
+  // function returnQueryObj(err, result){
+  //   if(err) reject(err);
+  //   else resolve(result);
+  // }
 
-  function renderWithQueryObj(err, result) {
-    if(err) console.error(err);
-    var tweets = result.rows;
-    res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
-  }
+  // function renderWithQueryObj(err, result) {
+  //   if(err) console.error(err);
+  //   var tweets = result.rows;
+  //   res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+  // }
 
   router.use(express.static('public/')); //typical use for express static middleware
   
   router.use(bodyParser.urlencoded({ extended: false })); //parses request body encoding
 
   router.get('/', function (req, res) {
-    promiseQuery('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id', null, renderWithQueryObj );
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id ORDER BY tweets.id', function (err, result) {
+      if(err) console.error(err);
+      var tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true});
+    });
   });
 
   router.get( '/users/:name', function (req, res, next){
-    promiseQuery('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id', [req.params.name], renderWithQueryObj );
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE name=$1 ORDER BY tweets.id', [req.params.name], function (err, result) {
+      if(err) console.error(err);
+      var tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+    });
   });
 
   router.get( '/tweet/:uniqID', function (req, res, next){
     var idNum = +req.params.uniqID
-    promiseQuery('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id=$1', [idNum], renderWithQueryObj );
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id=$1', [idNum], function (err, result) {
+      if(err) console.error(err);
+      var tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
+    });
   });
 
   router.post('/tweets', function (req, res) {
@@ -50,7 +70,11 @@ module.exports = function (io, client) {
 
     promiseQuery('SELECT * FROM users WHERE name=$1', [req.body.name])
     .then(function(data){
-      var queryObj = data.rowCount !== 0 ? data : promiseQuery('INSERT INTO users (name) VALUES ($1) RETURNING id', [req.body.name])
+      var queryObj = data.rowCount !== 0 ? data : promiseQuery('INSERT INTO users (name) VALUES ($1) RETURNING id', [req.body.name], function(err, result){
+      if(err) console.error(err);
+      var tweets = result.rows;
+      res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true }); 
+      });
       return queryObj;
     })
     .then(function(result){
@@ -69,6 +93,23 @@ module.exports = function (io, client) {
     .catch(function(err){
       console.error(err);
     });
+  });
+
+  router.post('/retweet', function (req, res) {
+
+    promiseQuery('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id=$1 ', [req.body.id])
+    .then(function(data){
+      client.query("INSERT INTO tweets (userid, content, parent) VALUES ($1, $2, $3)", [23, data.rows[0].content, data.rows[0].name], function (err, result) {
+        if(err) console.error(err);
+        io.sockets.emit('new_tweet', { name: "Kimber Kathy retweeted from: " + data.rows[0].name, text: data.rows[0].content});
+        res.redirect('/');
+      });
+    })
+    .catch(function(err){
+      console.error(err);
+    });
+
+    
   });
 
   return router;
