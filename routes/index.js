41c25abfe50d5070eeb7c1cc99953119e69568ea
Kimber Lynch <kmlynch714@gmail.com>
2016-05-03 11:22:56 -0400

    post using promises

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,25 @@ var router = express.Router();
 var path = require('path');
 var bodyParser = require('body-parser');
 
+
+
 module.exports = function (io, client) {
+
+  function promiseQuery(query, valueArray){
+    return new Promise(function (resolve, reject){
+      client.query(query, valueArray, function(err, result){
+        if(err) reject(err);
+        else resolve(result);
+      })
+    })
+  }
 
   router.use(express.static('public/')); //typical use for express static middleware
   
   router.use(bodyParser.urlencoded({ extended: false })); //parses request body encoding
 
   router.get('/', function (req, res) {
-    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id', function (err, result) { 
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id', function (err, result) {
       if(err) console.error(err);
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -22,7 +33,7 @@ module.exports = function (io, client) {
   });
 
   router.get( '/users/:name', function (req, res, next){
-    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE name=$1', [req.params.name], function (err, result) { 
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE name=$1', [req.params.name], function (err, result) {
       if(err) console.error(err);
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -31,7 +42,7 @@ module.exports = function (io, client) {
 
   router.get( '/tweet/:uniqID', function (req, res, next){
     var idNum = +req.params.uniqID
-    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id=$1', [idNum], function (err, result) { 
+    client.query('SELECT * FROM tweets INNER JOIN users ON tweets.userid = users.id WHERE tweets.id=$1', [idNum], function (err, result) {
       if(err) console.error(err);
       var tweets = result.rows;
       res.render('index', { title: 'Twitter.js', tweets: tweets, showForm: true });
@@ -41,34 +52,59 @@ module.exports = function (io, client) {
   router.post('/tweets', function (req, res) {
     //check if user exists (by name)
     var userID;
-    client.query('SELECT * FROM users WHERE name=$1', [req.body.name], function (err, result) {
-      if(result.rowCount === 0) {
-        client.query('INSERT INTO users (name) VALUES ($1) RETURNING id', [req.body.name], function (err, result) {
-          if(err) console.error(err);
-          console.log(result);
-          userID = result.rows[0].id;
-        });
-      } else {
-        userID = result.rows[0].id;
-      };
-      console.log(userID);
-      // client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [userID, req.body.text], function (err, result) { 
-      //   if(err) console.error(err);
-      //   io.sockets.emit('new_tweet', { name: req.body.name, text: req.body.text});
-      //   res.redirect('/');
-      // });
+
+    //WORKING POST USING CALLBACKS
+    //check if user exists
+    // client.query('SELECT * FROM users WHERE name=$1', [req.body.name], function (err, result) {
+    //   if(result.rowCount === 0) {
+    //     //if it doesn't exists add it into rows
+    //     client.query('INSERT INTO users (name) VALUES ($1) RETURNING id', [req.body.name], function (err, result) {
+    //       if(err) console.error(err);
+    //       userID = result.rows[0].id;
+    //         //insert new tweet using that id
+    //         client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [userID, req.body.text], function (err, result) {
+    //           if(err) console.error(err);
+    //           io.sockets.emit('new_tweet', { name: req.body.name, text: req.body.text});
+    //           res.redirect('/');
+    //         });
+    //     });
+    //   } else {
+    //     //if user does exist, insert that tweet using that id
+    //     userID = result.rows[0].id;
+    //     client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [userID, req.body.text], function (err, result) {
+    //       if(err) console.error(err);
+    //       io.sockets.emit('new_tweet', { name: req.body.name, text: req.body.text});
+    //       res.redirect('/');
+    //     });
+    //   }
+
+    // });
+    //END WORKING POST
+
+    promiseQuery('SELECT * FROM users WHERE name=$1', [req.body.name])
+    .then(function(data){
+      var queryObj = data.rowCount !== 0 ? data : promiseQuery('INSERT INTO users (name) VALUES ($1) RETURNING id', [req.body.name])
+      return queryObj;
+    })
+    .then(function(result){
+      console.log(result)
+      userID = result.rows[0].id;
+      return userID
+      //check if user exists
+    })
+    .then(function(data){
+      client.query('INSERT INTO tweets (userid, content) VALUES ($1, $2)', [data, req.body.text], function (err, result) {
+        if(err) console.error(err);
+        io.sockets.emit('new_tweet', { name: req.body.name, text: req.body.text});
+        res.redirect('/');
+      });
+    })
+    .catch(function(err){
+      console.error(err);
     });
-      //does not exist? insert user
-    //get user id by name
-    //insert into tweets by userid and content
-
-    //socket with name and content
-
-    
-
-    // tweetBank.add(req.body.name, req.body.text);
-    
   });
+
+
 
 
 
